a09da06f529409081114287e3aca79141df8fc37
Cristhiano Montanha <cmontanha@gmail.com>
2022-03-17 22:09:49 -0300

    Criando cadastro de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,15 +7,37 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $users = User::get();
-        
+
         return view('users.index', compact('users'));
     }
-    
-    public function show($id) {
-        dd('users.show',$id);
+
+    public function show($id)
+    {
+        // $user = User::where('id',$id)->first();
+
+        if (!$user = User::find($id))
+            return  redirect()->route('users.index');
+
+        return view('users.show', compact('user'));
+
+        // dd($user);
+
     }
 
-}
+    public function create(){
+        return view('users.create');
+    }
 
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+
+        $user = User::create($data);
+
+        return redirect()->route('users.index');
+    }
+}
